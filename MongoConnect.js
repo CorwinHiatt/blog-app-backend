d125e81ab872b4afc6f6f2119f4f5385cb7f5b28
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-11-21 16:01:37 -0500

    changed typo

--- MongoConnect.js
+++ MongoConnect.js
@@ -2,8 +2,8 @@ import { MongoClient } from 'mongodb'
 import 'dotenv/config'
 
 const client = new MongoClient(process.env.MONGO_URI)
-const database = client.db('blogs')
+const database = client.db('blog')
 export const posts = database.collection('posts')
 
 client.connect()
-console.log('connected to Mongo')
+console.log('connected to mongo')
